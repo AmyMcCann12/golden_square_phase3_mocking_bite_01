10814e44c3fb5b17c73a00f8c32333a382eee54d
AmyMcCann <amy.mccann.34@gmail.com>
2023-11-21 09:57:05 +0000

    Project folder created and completed initial set up

---
+++ lib/music_library.py
@@ -0,0 +1,17 @@
+class MusicLibrary:
+    # Public properties:
+    #   tracks: a list of instances of Track
+
+    def __init__(self):
+        pass
+
+    def add(self, track):
+        # track is an instance of Track
+        # Track gets added to the library
+        # Returns nothing
+        pass
+
+    def search(self, keyword):
+        # keyword is a string
+        # Returns a list of instances of track that match the keyword
+        pass
---
+++ lib/track.py
@@ -0,0 +1,9 @@
+class Track:
+    def __init__(self, title, artist):
+        # title and artist are both strings
+        pass
+
+    def matches(self, keyword):
+        # keyword is a string
+        # Returns true if the keyword matches either the title or artist
+        pass
